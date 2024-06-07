c4de77dcd597c570cbff3b283d56bc1d73f071d6
Trevor Brooks <trevorism@gmail.com>
2024-06-07 14:26:14 -0400

    Update encyption project, add test and deploy events

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 ![GitHub language count](https://img.shields.io/github/languages/count/trevorism/encryption)
 ![GitHub top language](https://img.shields.io/github/languages/top/trevorism/encryption)
 
+Encrypt and decrypt as a service. Bring your own key.
+
 Deployed to [Encryption](https://encryption.project.trevorism.com/)
 
-Current version 0-2-0
+Current version 0.3.0
 
 # How to build
 `gradle clean build`
--- src/acceptance/groovy/com/trevorism/gcloud/ContentRootDefinitions.groovy
+++ src/acceptance/groovy/com/trevorism/gcloud/ContentRootDefinitions.groovy
@@ -6,3 +6,33 @@ package com.trevorism.gcloud
 
 this.metaClass.mixin(io.cucumber.groovy.Hooks)
 this.metaClass.mixin(io.cucumber.groovy.EN)
+
+def contextRootContent
+def pingContent
+
+Given(~/^the application is alive$/) { ->
+    try{
+        new URL("https://deploy.project.trevorism.com/ping").text
+    }
+    catch (Exception ignored){
+        Thread.sleep(10000)
+        new URL("https://deploy.project.trevorism.com/ping").text
+    }
+}
+
+When(~/^I navigate to "([^"]*)"$/) { String url ->
+    contextRootContent = new URL(url).text
+}
+
+Then(~/^then a link to the help page is displayed$/) { ->
+    assert contextRootContent
+    assert contextRootContent.contains("/help")
+}
+
+When(~/^I ping the application deployed to "([^"]*)"$/) { String url ->
+    pingContent = new URL("${url}/ping").text
+}
+
+Then(~/^pong is returned, to indicate the service is alive$/) { ->
+    assert pingContent == "pong"
+}
